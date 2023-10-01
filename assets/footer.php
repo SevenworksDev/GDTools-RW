6657b961171658371e5eb663549517e8a4ed4b80
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-10-01 10:19:47 -0700

    Add files via upload

---
+++ assets/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+    <p>GDTools-RW is made by Sevenworks.EU.ORG and we will not login to your account without permission.</p>
+</div>
---
+++ assets/header.php
@@ -0,0 +1,9 @@
+<div class="header">
+    <div class="logo">
+        <span>GDTools-RW</span>
+    </div>
+    <ul class="nav">
+        <li><a href="/">Home</a></li>
+        <li><a href="/dashboard/dash.php">Dashboard</a></li>
+    </ul>
+</div>
